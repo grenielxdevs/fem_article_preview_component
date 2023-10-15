f6cd77acd834537f27849683a3726f44f795b3ac
Greniel Galinato <greniel.galinato@gmail.com>
2023-10-15 13:24:51 +0800

    fem_article_preview_component: adding toggle mode

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const shareBtn = document.querySelector('.share-btn');
 const share = document.querySelector('.share');
 const arrDown = document.querySelector('.arrow-down');
 
+let initState = false;
 
 shareBtn.addEventListener('click', function() {
-    share.classList.add('show');
-    arrDown.classList.add('show');
+    if(!initState) {
+        share.classList.add('show');
+        arrDown.classList.add('show');
+        shareBtn.classList.add('active')
+        initState = true;
+        return;
+    }
+
+    share.classList.remove('show');
+    arrDown.classList.remove('show');
+    shareBtn.classList.remove('active')
+    initState = false;
+
 })
